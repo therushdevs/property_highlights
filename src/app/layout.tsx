6be9a803f8fe59f3a50ed62c76a4975cd42f85a6
Rushikesh Kumbhar <mailmerpvk@gmail.com>
2024-10-03 23:01:32 +0530

    comment usecase

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <NotificationListener />
         <ToastProvider>{children}</ToastProvider>
       </body>
     </html>
